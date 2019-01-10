4672aad32044540fe25cfb83bc19be2fb2fb83af
isti <istikomahindah27@gmail.com>
2019-01-10 16:56:32 +0700

    Initial commit

---
+++ app/src/main/java/com/ti/febrian_hidayat/Rian.kt
@@ -0,0 +1,12 @@
+package com.ti.febrian_hidayat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Rian : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rian)
+    }
+}
